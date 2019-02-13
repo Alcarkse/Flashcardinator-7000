98f240c8edcd1260e1c78a77792048f648a99573
Alcarkse <15620365+Alcarkse@users.noreply.github.com>
2019-02-13 22:01:53 +0000

    Further style progess

--- chineseVoc/Flashcards.php
+++ chineseVoc/Flashcards.php
@@ -1,11 +1,21 @@
 <?php
-$lang = $_GET["lang"];
-$cookieExists = true;
+    $lang = $_GET["lang"];
+    $cookieExists = true;
+    $vocab;
 
-if($cookieExists == false){
-    header('location: Login.php');
-}
-
+    if($cookieExists == false){
+        header('location: Login.php');
+    }
 ?>
 
-<h2>Flashcards</h2>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'common/headStuff.html'; ?>
+    </head>
+    <body>
+        <h1 id="title">Flashcardinator-700</h1>
+        <h2>Flashcards</h2>
+        
+    </body>
+</html>
--- chineseVoc/index.php
+++ chineseVoc/index.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="_css/main.css">
-    <title>Flashcardinator-700</title>
-</head>
+    <head>
+        <?php include 'common/headStuff.html'; ?>
+    </head>
 
-<body>
-<h1 id="title">Flashcardinator-700</h1>
-<div id="languageSelection">
-<a href="Flashcards.php?lang=zh">
-    <button class="highlightButton" id="zh">中国语</button>    
-</a>
-<a href="Flashcards.php?lang=jp">
-    <button class="highlightButton" id="jp">日本語</button>
-</a>
-</div>
-</body>
+    <body>
+        <h1 id="title">Flashcardinator-700</h1>
+        <div id="languageSelection">
+            <a href="Flashcards.php?lang=zh">
+                <button class="highlightButton" id="zh">中国语</button>    
+            </a>
+            <a href="Flashcards.php?lang=jp">
+                <button class="highlightButton" id="jp">日本語</button>
+            </a>
+        </div>
+    </body>
 
 </html>
