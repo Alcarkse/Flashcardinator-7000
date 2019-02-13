8d94d4948ea3dec9fd7a7cc9b60bae5516cb274f
Alcarkse <15620365+Alcarkse@users.noreply.github.com>
2019-02-13 21:07:38 +0000

    Basic Index Style

--- chineseVoc/index.php
+++ chineseVoc/index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="_css/main.css">
     <title>Document</title>
 </head>
+
 <body>
-    <button class="highlightButton" id="zh">中国语</button>
-    <button class="highlightButton" id="jp">日本語</button>
+    <div id="languageSelection">
+        <button class="highlightButton" id="zh" lang="zh">中国语</button>
+        <button class="highlightButton" id="jp" lang="jp">日本語</button>
+    </div>
 </body>
+
 </html>
