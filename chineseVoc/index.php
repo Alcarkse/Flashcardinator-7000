0f861d63bc7827ca0cd52c02ff35cc452dca2620
Alcarkse <15620365+Alcarkse@users.noreply.github.com>
2019-02-13 21:18:12 +0000

    Styling progress

--- chineseVoc/index.php
+++ chineseVoc/index.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="_css/main.css">
-    <title>Document</title>
+    <title>Flashcardinator-700</title>
 </head>
 
 <body>
+
+    <h1 id="title">Flashcardinator-700</h1>
     <div id="languageSelection">
         <button class="highlightButton" id="zh" lang="zh">中国语</button>
         <button class="highlightButton" id="jp" lang="jp">日本語</button>
